zimbatm/flake-modules-core

Deploy basic site with netlify

dev/flake-module.nix
@@ -3,6 +3,7 @@ flakeModuleArgs@{ config, lib, inputs, ... }:
 {
   imports = [
     inputs.pre-commit-hooks-nix.flakeModule
+    ../site/flake-module.nix
   ];
   systems = [ "x86_64-linux" "aarch64-darwin" ];
   perSystem = system: { config, self', inputs', pkgs, ... }: {
@@ -12,6 +13,9 @@ flakeModuleArgs@{ config, lib, inputs, ... }:
       nativeBuildInputs = [
         pkgs.nixpkgs-fmt
         pkgs.pre-commit
+        pkgs.hci
+        pkgs.netlify-cli
+        pkgs.pandoc
       ];
       shellHook = ''
         ${config.pre-commit.installationScript}
@@ -43,9 +47,23 @@ flakeModuleArgs@{ config, lib, inputs, ... }:
   };
   flake = {
     options.herculesCI = lib.mkOption { type = lib.types.raw; };
-    config.herculesCI = {
+    config.herculesCI = { branch, ... }: {
       onPush.default.outputs = {
         inherit (config.flake) packages checks;
+        effects =
+          let
+            pkgs = inputs.nixpkgs.legacyPackages.x86_64-linux;
+            effects = inputs.hercules-ci-effects.lib.withPkgs pkgs;
+          in
+          {
+            netlifyDeploy = effects.runIf (branch == "main") (effects.netlifyDeploy {
+              websitePackage = config.flake.packages.x86_64-linux.websitePackage;
+              secretName = "default-netlify";
+              secretData = "authToken";
+              siteId = "29a153b1-3698-433c-bc73-62415efb8117";
+              productionDeployment = true;
+            });
+          };
       };
     };
 

hercules-ci/flake-parts

dev/flake-module.nix
@@ -3,6 +3,7 @@ flakeModuleArgs@{ config, lib, inputs, ... }:
 {
   imports = [
     inputs.pre-commit-hooks-nix.flakeModule
+    ../site/flake-module.nix
   ];
   systems = [ "x86_64-linux" "aarch64-darwin" ];
   perSystem = system: { config, self', inputs', pkgs, ... }: {
@@ -11,6 +12,9 @@ flakeModuleArgs@{ config, lib, inputs, ... }:
       nativeBuildInputs = [
         pkgs.nixpkgs-fmt
         pkgs.pre-commit
+        pkgs.hci
+        pkgs.netlify-cli
+        pkgs.pandoc
       ];
       shellHook = ''
         ${config.pre-commit.installationScript}
@@ -43,9 +47,22 @@ flakeModuleArgs@{ config, lib, inputs, ... }:
   };
   flake = {
     options.herculesCI = lib.mkOption { type = lib.types.raw; };
-    config.herculesCI = {
+    config.herculesCI = { branch, ... }: {
       onPush.default.outputs = {
         inherit (config.flake) packages checks;
+        effects =
+          let
+            pkgs = inputs.nixpkgs.legacyPackages.x86_64-linux;
+            effects = inputs.hercules-ci-effects.lib.withPkgs pkgs;
+          in
+          {
+            netlifyDeploy = effects.runIf (branch == "main") (effects.netlifyDeploy {
+              content = config.flake.packages.x86_64-linux.websitePackage;
+              secretName = "default-netlify";
+              siteId = "29a153b1-3698-433c-bc73-62415efb8117";
+              productionDeployment = true;
+            });
+          };
       };
     };
 