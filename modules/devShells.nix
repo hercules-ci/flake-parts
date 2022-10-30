5e259eb312e19f1a961fae9e3d971f15567f2c0d
Shea Levy <shea@shealevy.com>
2022-10-30 07:01:01 -0400

    devShells: Use mkTransposedPerSystemModule

--- modules/devShells.nix
+++ modules/devShells.nix
@@ -1,45 +1,22 @@
 { config, lib, flake-parts-lib, ... }:
 let
   inherit (lib)
-    filterAttrs
-    mapAttrs
     mkOption
-    optionalAttrs
     types
     ;
   inherit (flake-parts-lib)
-    mkSubmoduleOptions
-    mkPerSystemOption
+    mkTransposedPerSystemModule
     ;
 in
-{
-  options = {
-    flake = mkSubmoduleOptions {
-      devShells = mkOption {
-        type = types.lazyAttrsOf (types.lazyAttrsOf types.package);
-        default = { };
-        description = ''
-          Per system an attribute set of packages used as shells.
-          <literal>nix develop .#&lt;name></literal> will run <literal>devShells.&lt;system>.&lt;name></literal>.
-        '';
-      };
-    };
-
-    perSystem = mkPerSystemOption
-      ({ config, system, ... }: {
-        options = {
-          devShells = mkOption {
-            type = types.lazyAttrsOf types.package;
-            default = { };
-            description = ''
-              An attribute set of packages to be built by <literal>nix develop .#&lt;name></literal>.
-              <literal>nix build .#&lt;name></literal> will run <literal>devShells.&lt;name></literal>.
-            '';
-          };
-        };
-      });
+mkTransposedPerSystemModule {
+  name = "devShells";
+  option = mkOption {
+    type = types.lazyAttrsOf types.package;
+    default = { };
+    description = ''
+      An attribute set of packages to be built by <literal>nix develop .#&lt;name></literal>.
+      <literal>nix build .#&lt;name></literal> will run <literal>devShells.&lt;name></literal>.
+    '';
   };
-  config = {
-    transposition.devShells = { };
-  };
+  file = ./devShells.nix;
 }
