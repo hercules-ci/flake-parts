e7da962abb2fa053a2601211a0e56ded9ac779d1
Shea Levy <shea@shealevy.com>
2022-10-30 07:03:32 -0400

    formatter: Use mkTransposedPerSystemModule

--- modules/formatter.nix
+++ modules/formatter.nix
@@ -1,55 +1,21 @@
 { config, lib, flake-parts-lib, ... }:
 let
   inherit (lib)
-    filterAttrs
-    mapAttrs
     mkOption
-    optionalAttrs
     types
     ;
   inherit (flake-parts-lib)
-    mkSubmoduleOptions
-    mkPerSystemOption
+    mkTransposedPerSystemModule
     ;
 in
-{
-  options = {
-    flake = mkSubmoduleOptions {
-      formatter = mkOption {
-        type = types.lazyAttrsOf types.package;
-        default = { };
-        description = ''
-          Per system package used by <literal>nix fmt</literal>.
-        '';
-      };
-    };
-
-    perSystem = mkPerSystemOption ({ config, ... }: {
-      _file = ./formatter.nix;
-      options = {
-        formatter = mkOption {
-          type = types.nullOr types.package;
-          default = null;
-          description = ''
-            A package used by <literal>nix fmt</literal>.
-          '';
-        };
-      };
-    });
+mkTransposedPerSystemModule {
+  name = "formatter";
+  option = mkOption {
+    type = types.nullOr types.package;
+    default = null;
+    description = ''
+      A package used by <literal>nix fmt</literal>.
+    '';
   };
-  config = {
-    flake.formatter =
-      mapAttrs
-        (k: v: v.formatter)
-        (filterAttrs
-          (k: v: v.formatter != null)
-          config.allSystems
-        );
-
-    perInput = system: flake:
-      optionalAttrs (flake?formatter.${system}) {
-        formatter = flake.formatter.${system};
-      };
-
-  };
+  file = ./formatter.nix;
 }
