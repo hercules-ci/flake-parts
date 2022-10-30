5bcff37bc172020c3fa26f6ba4bab537c8949133
Shea Levy <shea@shealevy.com>
2022-10-30 06:58:53 -0400

    checks: Use mkTransposedPerSystemModule

--- modules/checks.nix
+++ modules/checks.nix
@@ -1,44 +1,21 @@
 { config, lib, flake-parts-lib, ... }:
 let
   inherit (lib)
-    filterAttrs
-    mapAttrs
     mkOption
-    optionalAttrs
     types
     ;
   inherit (flake-parts-lib)
-    mkSubmoduleOptions
-    mkPerSystemOption
+    mkTransposedPerSystemModule
     ;
 in
-{
-  options = {
-    flake = mkSubmoduleOptions {
-      checks = mkOption {
-        type = types.lazyAttrsOf (types.lazyAttrsOf types.package);
-        default = { };
-        description = ''
-          Derivations to be built by nix flake check.
-        '';
-      };
-    };
-
-    perSystem = mkPerSystemOption ({ config, system, ... }: {
-      _file = ./checks.nix;
-      options = {
-        checks = mkOption {
-          type = types.lazyAttrsOf types.package;
-          default = { };
-          description = ''
-            Derivations to be built by nix flake check.
-          '';
-        };
-      };
-    });
-
+mkTransposedPerSystemModule {
+  name = "checks";
+  option = mkOption {
+    type = types.lazyAttrsOf types.package;
+    default = { };
+    description = ''
+      Derivations to be built by nix flake check.
+    '';
   };
-  config = {
-    transposition.checks = { };
-  };
+  file = ./checks.nix;
 }
