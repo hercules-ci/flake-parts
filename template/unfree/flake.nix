b3d11ffcb4e367169d3677adf1ec0746963734b9
Jörg Thalheim <joerg@thalheim.io>
2023-03-25 07:38:31 +0100

    add an unfree template

---
+++ template/unfree/flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "Description for the project";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = inputs@{ flake-parts, nixpkgs, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" "aarch64-darwin" ];
+      perSystem = { pkgs, system, ... }: {
+        # This sets `pkgs` to a nixpkgs with allowUnfree option set.
+        _module.args.pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+
+        packages.default = pkgs.hello-unfree;
+      };
+    };
+}
