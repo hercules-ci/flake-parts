bf47eae50e9461497be2ccf379cfd74526d6745e
Dusk Banks <me@bb010g.com>
2023-04-19 01:31:39 +0000

    treewide: Normalize warning & error messages

--- modules/moduleWithSystem.nix
+++ modules/moduleWithSystem.nix
@@ -9,13 +9,14 @@
         let
           system =
             config._module.args.system or
-              config._module.args.pkgs.stdenv.hostPlatform.system or
-                (throw "moduleWithSystem: Could not determine the configuration's system parameter for this module system application.");
+              config._module.args.pkgs.stdenv.hostPlatform.system or (throw
+                "moduleWithSystem: Could not determine the `system` parameter for this module set evaluation."
+              );
 
           allArgs = withSystem system (args: args);
 
           lazyArgsPerParameter = f: builtins.mapAttrs
-            (k: v: allArgs.${k} or (throw "moduleWithSystem: module argument `${k}` does not exist."))
+            (k: v: allArgs.${k} or (throw "moduleWithSystem: per-system argument `${k}` does not exist."))
             (builtins.functionArgs f);
 
           # Use reflection to make the call lazy in the argument.
