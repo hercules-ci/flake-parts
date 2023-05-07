84986d2b8851aade5110f18655ab336315f66926
Yang, Bo <pop.atry@gmail.com>
2023-05-06 20:05:15 -0700

    Use root nixpkgs by default

--- extras/nixpkgs.nix
+++ extras/nixpkgs.nix
@@ -1,7 +1,7 @@
 {inputs, flake-parts-lib, ...}: {
   options.perSystem = mkPerSystemOption ({ pkgs, system, ... }: {
     imports = [
-      "${(flake-parts-lib.findInputByOutPath ./. inputs).nixpkgs}/nixos/modules/misc/nixpkgs.nix"
+      "${inputs.nixpkgs or (flake-parts-lib.findInputByOutPath ./. inputs).nixpkgs}/nixos/modules/misc/nixpkgs.nix"
     ];
     nixpkgs.hostPlatform = system;
   });
