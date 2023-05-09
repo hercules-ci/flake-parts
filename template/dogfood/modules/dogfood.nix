d30b7b26d0d08b540db9a9647171020305b5e026
Yang, Bo <pop.atry@gmail.com>
2023-05-09 00:33:48 -0700

    Simplify dogfood.nix

--- template/dogfood/modules/dogfood.nix
+++ template/dogfood/modules/dogfood.nix
@@ -1,32 +1,23 @@
-{flake-parts-lib, lib, inputs, ...}: {
+{flake-parts-lib, lib, inputs, ...}@topLevel: {
   imports = [
     ./hello.nix
     inputs.flake-parts.flakeModules.flakeModules
   ];
-  config.systems = [ "x86_64-linux" "aarch64-darwin" ];
-  options.flake = lib.mkOption {
-    type = lib.types.submoduleWith {
-      modules = [
-        (flake: {
-          flakeModules.dogfood = {
-            config.systems = [ "x86_64-linux" "aarch64-darwin" ];
-            imports = [
-              flake.config.flakeModules.hello
+  flake.flakeModules.dogfood = {
+    config.systems = [ "x86_64-linux" "aarch64-darwin" ];
+    imports = [
+      topLevel.config.flake.flakeModules.hello
 
-              # Expose flake modules
-              ./dogfood.nix
-              ./hello.nix
-              ./anotherFlakeModule.nix
-            ];
-            options.perSystem = flake-parts-lib.mkPerSystemOption (perSystem: {
-              apps.default = {
-                type = "app";
-                program = "${perSystem.config.packages.hello_22_11}/bin/hello";
-              };
-            });
-          };
-        })
-      ];
-    };
+      # Expose flake modules
+      ./dogfood.nix
+      ./hello.nix
+      ./anotherFlakeModule.nix
+    ];
+    options.perSystem = flake-parts-lib.mkPerSystemOption (perSystem: {
+      apps.default = {
+        type = "app";
+        program = "${perSystem.config.packages.hello_22_11}/bin/hello";
+      };
+    });
   };
 }
