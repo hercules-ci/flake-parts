a13fbf2b39fff53cc09421c969b8eb9851eda998
Yang, Bo <pop.atry@gmail.com>
2023-05-08 19:18:57 -0700

    Create a more sophisticated lazy self

--- template/dogfood/flake.nix
+++ template/dogfood/flake.nix
@@ -5,12 +5,17 @@
     nixpkgs_22_11.url = "github:NixOS/nixpkgs/nixos-22.11";
   };
 
-  outputs = { flake-parts, ... }@inputs:
+  outputs = { flake-parts, self, ... }@inputs:
     flake-parts.lib.mkFlake { inherit inputs; } (
       flake-parts.lib.mkFlake
         {
+          inherit inputs;
           # Workaround for https://github.com/hercules-ci/flake-parts/issues/148
-          inputs = inputs // { self.outPath = ./.; };
+          self = {
+            outPath = ./.;
+            inherit (self)
+              _type inputs lastModified lastModifiedDate narHash outputs sourceInfo submodules;
+          };
         }
         ./modules/dogfood.nix
     ).flakeModules.dogfood;
