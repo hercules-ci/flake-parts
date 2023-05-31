904937e5f4058f1a760b975e1983af8d5786056d
Robert Hensing <robert@roberthensing.nl>
2023-05-31 14:11:00 +0200

    Add comment to nixpkgs-lib dependency

--- flake.nix
+++ flake.nix
@@ -2,6 +2,15 @@
   description = "Flake basics described using the module system";
 
   inputs = {
+    # nix-community/nixpkgs.lib is equivalent to nixpkgs?dir=lib, except it is
+    # decoupled from its parent directory.
+    #
+    # You may use nixpkgs?dir=lib or even nixpkgs as an input instead, using follows.
+    # See https://nixos.org/manual/nix/stable/command-ref/new-cli/nix3-flake.html#flake-inputs
+    #
+    # Ideally fetching a subtree would be an builtins.fetchTree (aka flake inputs)
+    # feature, but that has not been implemented yet, and while take a while to
+    # be widely available.
     nixpkgs-lib.url = "github:nix-community/nixpkgs.lib";
   };
 
