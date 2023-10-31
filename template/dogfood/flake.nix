89c069df9e14048c89b0af4a65b7aeb2a034e98f
Yang, Bo <bo@preemo.io>
2023-10-31 23:16:06 +0000

    Add bootstrap step

--- template/dogfood/flake.nix
+++ template/dogfood/flake.nix
@@ -5,13 +5,14 @@
     nixpkgs_22_11.url = "github:NixOS/nixpkgs/nixos-22.11";
   };
 
-  outputs = { flake-parts, self, ... }@inputs:
-    flake-parts.lib.mkFlake { inherit inputs; } (
+  outputs = { flake-parts, self, ... }@inputs: let
+    bootstrap = 
       flake-parts.lib.mkFlake
         {
           inherit inputs;
           moduleLocation = ./flake.nix;
         }
-        ./modules/dogfood.nix
-    ).flakeModules.dogfood;
+        ./modules/dogfood.nix;
+  in
+    flake-parts.lib.mkFlake { inherit inputs; } bootstrap.flakeModules.dogfood;
 }
