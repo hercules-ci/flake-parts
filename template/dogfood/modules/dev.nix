478396adb9ca44604afeb5812c74f007d86a8260
Yang, Bo <pop.atry@gmail.com>
2023-11-03 11:21:50 -0700

    Update dev.nix

--- template/dogfood/modules/dev.nix
+++ template/dogfood/modules/dev.nix
@@ -3,16 +3,18 @@ rec {
   imports = [
     inputs.flake-parts.flakeModules.flakeModules
     flake.flakeModules.dev
+
+    # Use file name to dogfood a flake module defined from the current flake to avoid infinite recursion
+    ./hello.nix
   ];
   flake.flakeModules.dev = dev: {
-    config.systems = [ "x86_64-linux" "aarch64-darwin" ];
-    imports = [
-      (
-        if topLevel.moduleLocation == dev.moduleLocation
-        then ./hello.nix # Use file name to dogfood a flake module defined from the current flake to avoid infinite recursion
-        else topLevel.config.flake.flakeModules.hello # Use attributes to reference the flake module from other flakes
-      )
+    imports = lib.lists.optionals (topLevel.moduleLocation != dev.moduleLocation) [
+      # Use attributes to reference the flake module from other flakes
+      topLevel.config.flake.flakeModules.hello
     ];
+
+    config.systems = [ "x86_64-linux" "aarch64-darwin" ];
+
     options.perSystem = flake-parts-lib.mkPerSystemOption ({pkgs, ...}@perSystem: {
       devShells.default = pkgs.mkShell {
         buildInputs = [ perSystem.config.packages.hello_22_11 ];
