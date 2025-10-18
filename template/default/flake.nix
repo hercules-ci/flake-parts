7829070bcff8c14afedee24a43b4118e33cf2f19
Michael Vorburger <mike@vorburger.ch>
2025-10-18 13:34:14 +0200

    docs: Clarify internal vs. external import in template
    I'm a Nix noob and just learning, but was a little bit confused about this at first; perhaps this helps to clarify it for others who will come along in the future.

--- template/default/flake.nix
+++ template/default/flake.nix
@@ -9,10 +9,11 @@
   outputs = inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       imports = [
-        # To import a flake module
-        # 1. Add foo to inputs
-        # 2. Add foo as a parameter to the outputs function
-        # 3. Add here: foo.flakeModule
+        # To import an internal flake module: ./other.nix
+        # To import an external flake module:
+        #   1. Add foo to inputs
+        #   2. Add foo as a parameter to the outputs function
+        #   3. Add here: foo.flakeModule
 
       ];
       systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
