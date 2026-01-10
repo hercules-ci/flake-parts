hercules-ci/flake-parts

commit fcf9d5234bea80b07aebf98a7893c48a201c4f46

dev: Add pre-commit-hooks.nix

dev/flake-module.nix
@@ -1,14 +1,28 @@
-flakeModuleArgs@{ config, lib, ... }:
+flakeModuleArgs@{ config, lib, inputs, ... }:
 
 {
   imports = [
+    inputs.pre-commit-hooks-nix.flakeModule
   ];
   systems = [ "x86_64-linux" "aarch64-darwin" ];
   perSystem = system: { config, self', inputs', pkgs, ... }: {
     _module.args.pkgs = inputs'.nixpkgs.legacyPackages;
+
     devShells.default = pkgs.mkShell {
-      nativeBuildInputs = [ pkgs.nixpkgs-fmt ];
+      nativeBuildInputs = [
+        pkgs.nixpkgs-fmt
+        pkgs.pre-commit
+      ];
+      shellHook = ''
+        ${config.pre-commit.installationScript}
+      '';
     };
+    pre-commit = {
+      inherit pkgs; # should make this default to the one it can get via follows
+      settings = {
+      };
+    };
+
     packages = {
       inherit (pkgs.nixosOptionsDoc { inherit (flakeModuleArgs) options; })
         optionsDocBook;

dev/default.nix
@@ -1,9 +1,12 @@
 let
   flake = builtins.getFlake (toString ./.);
   fmc-lib = import ../lib.nix { inherit (flake.inputs.nixpkgs) lib; };
-  self = { inherit (flake) inputs; } // 
-    fmc-lib.evalFlakeModule
-      { inherit self; }
-      ./flake-module.nix;
+  self = {
+    inherit (flake) inputs;
+    outPath = ../.; # used by pre-commit module, etc
+  } //
+  fmc-lib.evalFlakeModule
+    { inherit self; }
+    ./flake-module.nix;
 in
   self.config.flake // { inherit (flake) inputs; }

dev/flake.nix
@@ -5,6 +5,9 @@
     # Flakes don't give us a good way to depend on .., so we don't.
     # This has drastic consequences of course.
     nixpkgs.url = "github:hercules-ci/nixpkgs/functionTo-properly";
+
+    pre-commit-hooks-nix.url = "github:hercules-ci/pre-commit-hooks.nix/flakeModule";
+    pre-commit-hooks-nix.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = { self, ... }:

commit 2872f976ba7f8702b464a84ba23b63dc01e41df3

dev/flake-module: delegate pre-commit installation to upstream
Fixes: fcf9d5234bea ("dev: Add pre-commit-hooks.nix")

dev/flake-module.nix
@@ -22,11 +22,10 @@
     devShells.default = pkgs.mkShell {
       nativeBuildInputs = [
         pkgs.nixpkgs-fmt
-        pkgs.pre-commit
         pkgs.hci
       ];
       shellHook = ''
-        ${config.pre-commit.installationScript}
+        ${config.pre-commit.shellHook}
       '';
     };
 