5d1243f12f2e4f2065bfa9021d7f6947d6c383d7
removed-user <caseyriley94@gmail.com>
2026-06-17 18:19:27 -0400

    Update documentation for flakeModules
    Clarify the description of flakeModules, their behavior, their purpose, how they differ from standard modules, and how to enable them

--- extras/flakeModules.nix
+++ extras/flakeModules.nix
@@ -19,9 +19,22 @@ let
       _class = "flake";
     });
     description = ''
-      flake-parts modules for use by other flakes.
+      flake-parts "modules" for use by other flakes.
 
-      If the flake defines only one module, it should be `flakeModules.default`.
+      If the flake defines only one flakeModule, it should be `flakeModules.default`.
+
+      These are similar to standard flake-parts "modules", aside from being type "deferredModule";
+      Which importantly, means they
+      - are able to defer function evaluation
+      - are not "evaluated eagerly"
+      - are not type-checked, and merged in the same way as other module types
+      - can create a new, named - top-level output, like a new `apps, programs, devshells`, etc. Such as "myLib", where
+           - A: you define all of the types of any internal options
+           - B: all of the merge rules for them
+           - C: Can ignore the options system completely
+           - D: Can export your own flake.lib functions directly under your "flakes namespace" So that others can reference it via yourflake.lib
+
+      - To enable this, you should import `inputs.flake-parts.flakeModules.flakeModules`, 
 
       You can not read this option in defining the flake's own `imports`. Instead, you can
       put the module in question into its own file or let binding and reference
